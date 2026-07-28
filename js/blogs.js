52b652881817cb99954b5ca77eef28d07ed9c8e0
Grey-Hat-Guy <greyhat2703@gmail.com>
2026-07-28 19:14:05 +0530

    URL change

--- js/blogs.js
+++ js/blogs.js
@@ -1,4 +1,4 @@
-const API_URL = "http://elitser-me.com/etlblogs/wp-json/wp/v2/posts";
+const API_URL = "https://elitser-me.com/etlblogs/wp-json/wp/v2/posts";
 const POSTS_PER_PAGE = 6;
 
 let allPosts = [];
